24198a971172d3b8092a6fcc5dede08ed135f346
Anh Tran <anhtnt@elightup.com>
2020-06-23 11:12:32 +0700

    Bold tên field

--- src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
+++ src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
@@ -15,13 +15,13 @@ const FieldSelected = (props) => {
       onDragLeave={props.onDragLeave}
       className={`og-item ${props.dragAndDrop && props.dragAndDrop.draggedTo === Number(props.index) && "dropArea"}`}>
       <div className="og-item__heading" onClick={() => props.handleShow(props.index + 1)}>
-        {props.data?.general?.name}
-        <span className="og-item__actions">
+        <div class="og-item__title">{props.data?.general?.name}</div>
+        <div className="og-item__actions">
           <span className="og-item__type">{props.data?.general?.type}</span>
           <span className="og-item__action og-item__action--remove" title="Remove" onClick={() => props.removeItem(props.index)}>{trashIcon}</span>
           <span className="og-item__action og-item__action--duplicate" title="Duplicate" onClick={() => props.copyItem(props.data?.general?.type, props.index)} >{copyIcon}</span>
           <span className="og-item__action og-item__action--toggle" title="Toggle Settings">{props.isShow ? arrowUpIcon : arrowDownIcon}</span>
-        </span>
+        </div>
       </div>
       { props.isShow &&
         <div className="og-item__body">
