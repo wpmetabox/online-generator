8722527522b72e436d1a2fb83e6a41d5eb9762ba
Anh Tran <anhtnt@elightup.com>
2020-07-06 17:21:49 +0700

    Sửa input type cho Id và Desc

--- src/components/Common/Elements/Name.js
+++ src/components/Common/Elements/Name.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Input from '../Input';
 
-const Name = ({...props}) => <Input {...props} label="Label" tooltip="Optional field label. If empty, the field input is 100% width." />
+const Name = ({...props}) => <Input {...props} label="Label" type="text" tooltip="Optional field label. If empty, the field input is 100% width." />
 
 export default Name;
