ac448f69739adee4576378bf8c2977870557d2f0
Anh Tran <anhtnt@elightup.com>
2020-07-13 10:16:16 +0700

    Done file advanced

---
+++ src/components/Common/Elements/MaxStatus.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Checkbox from '../Checkbox';
+
+const MaxStatus = props => <Checkbox {...props} label="Show status" tooltip="Display how many files uploaded/remaining" />
+export default MaxStatus;
