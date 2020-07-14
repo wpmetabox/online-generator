391ef15f83705182d8d32fde0310cbc5fe7f7036
Anh Tran <anhtnt@elightup.com>
2020-07-14 10:52:39 +0700

    Add autosave

--- src/components/MainTabs/Tabs/SettingsTab.js
+++ src/components/MainTabs/Tabs/SettingsTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Input from '../../Common/Input';
+import Checkbox from '../../Common/Checkbox';
 import DivRow from '../../Common/DivRow';
 
 const SettingsTab = ({register}) => {
@@ -24,6 +25,7 @@ const SettingsTab = ({register}) => {
         <label><input ref={register} type="radio" name="priority" value="high" defaultChecked /> High</label>
         <label><input ref={register} type="radio" name="priority" value="low" /> Low</label>
       </DivRow>
+      <Checkbox register={register} name="autosave" label="Autosave" />
       <h3>Advanced</h3>
       <Input register={register} name="prefix" label="Field ID prefix" defaultValue="prefix-" description="Prefix for all fields' ID. Leave empty to ignore or use _ to make fields hidden." tooltip="Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins." />
       <Input register={register} name="text_domain" label="Text domain" defaultValue="online-generator" tooltip="Required for multilingual website. Used in the exported code only." />
