e202c7de3eef20c79a010fbd33143a2994406d83
Anh Tran <anhtnt@elightup.com>
2020-07-15 10:18:14 +0700

    Update lại field label khi xoá, duplicate

--- src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
+++ src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import { trashIcon, copyIcon, arrowDownIcon, arrowUpIcon } from '../../../../constants/icons';
-import { TabPanel, Tabs, TabList, Tab } from 'react-tabs';
+import React, {useState, useEffect} from 'react';
+import {trashIcon, copyIcon, arrowDownIcon, arrowUpIcon} from '../../../../constants/icons';
+import {TabPanel, Tabs, TabList, Tab} from 'react-tabs';
 import GeneralContent from './FieldContent/GeneralContent';
 import AdvancedContent from './FieldContent/AdvancedContent';
 import {ucfirst} from '../../../../utility/functions';
@@ -8,9 +8,15 @@ import {ucfirst} from '../../../../utility/functions';
 const FieldSelected = props => {
   const type = props.data.general.type;
   const index = props.index;
-  const [label, setLabel] = useState(props.data?.general?.name);
+
   const [expanded, setExpanded] = useState(false);
   const toggleSettings = () => setExpanded(!expanded);
+
+  const [label, setLabel] = useState(props.data?.general?.name);
+  useEffect(() => {
+    const element = document.querySelector( `#fields-${index}-name` );
+    setLabel( element?.value );
+  });
 
   if ('divider' === type) {
     return (
@@ -22,7 +28,8 @@ const FieldSelected = props => {
         onDrop={props.onDrop}
         onDragLeave={props.onDragLeave}
         className={`og-item og-collapsible${expanded ? ' og-collapsible--expanded' : ''} ${props.dragAndDrop && props.dragAndDrop.draggedTo === Number(index) && "dropArea"}`}>
-        <Header {...props} label={label} expanded={expanded} toggleSettings={toggleSettings} />
+        <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
+        <Header type={type} index={index} label={label} expanded={expanded} copyItem={props.copyItem} removeItem={props.removeItem} toggleSettings={toggleSettings} />
         <div className="og-item__body og-collapsible__body">
           <GeneralContent register={props.register} type={type} index={index} fieldData={props.data.general} />
         </div>
@@ -39,7 +46,8 @@ const FieldSelected = props => {
       onDrop={props.onDrop}
       onDragLeave={props.onDragLeave}
       className={`og-item og-collapsible${expanded ? ' og-collapsible--expanded' : ''} ${props.dragAndDrop && props.dragAndDrop.draggedTo === Number(index) && "dropArea"}`}>
-      <Header {...props} label={label} expanded={expanded} toggleSettings={toggleSettings} />
+      <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
+      <Header type={type} index={index} label={label} expanded={expanded} copyItem={props.copyItem} removeItem={props.removeItem} toggleSettings={toggleSettings} />
       <div className="og-item__body og-collapsible__body">
         <Tabs forceRenderTabPanel={true}>
           <TabList>
@@ -58,24 +66,20 @@ const FieldSelected = props => {
   )
 }
 const Header = props => {
-  const type = props.data.general.type;
-  const index = props.index;
-
   const duplicate = e => {
     e.stopPropagation();
-    props.copyItem(type, index);
+    props.copyItem(props.type, props.index);
   }
   const remove = e => {
     e.stopPropagation();
-    props.removeItem(index);
+    props.removeItem(props.index);
   }
 
   return (
     <div className="og-item__header og-collapsible__header" onClick={props.toggleSettings}>
-      <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
-      <div className="og-item__title">{props.label || ucfirst(type)}</div>
+      <div className="og-item__title">{props.label || ucfirst(props.type)}</div>
       <div className="og-item__actions">
-        <span className="og-item__type">{type}</span>
+        <span className="og-item__type">{props.type}</span>
         <span className="og-item__action og-item__action--remove" title="Remove" onClick={remove}>{trashIcon}</span>
         <span className="og-item__action og-item__action--duplicate" title="Duplicate" onClick={duplicate}>{copyIcon}</span>
         <span className="og-item__action og-item__action--toggle" title="Toggle Settings">{props.expanded ? arrowUpIcon : arrowDownIcon}</span>
