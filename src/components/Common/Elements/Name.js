5a65f05f8e9d3359b2d14dbbc5c36ff1a2bc7ab5
Anh Tran <anhtnt@elightup.com>
2020-07-18 10:00:35 +0700

    Fix set field name when changing

--- src/components/Common/Elements/Name.js
+++ src/components/Common/Elements/Name.js
@@ -2,13 +2,11 @@ import React from 'react';
 import DivRow from '../DivRow';
 
 const Name = ({ name, defaultValue, register, ...rest }) => {
-  const changeFieldLabel = label => {
-    document.getElementById(`og-item__title__${rest.data.id}`).text(label)
-  }
+  const changeFieldLabel = e => document.getElementById(`og-item__title__${rest.data.id}`).textContent = e.target.value;
 
   return (
     <DivRow htmlFor={name} {...rest} label="Label" tooltip="Optional field label. If empty, the field input is 100% width.">
-      <input type="text" id={name} name={name} ref={register} defaultValue={defaultValue} onChange={e => changeFieldLabel(e.target.value)} />
+      <input type="text" id={name} name={name} ref={register} defaultValue={defaultValue} onChange={changeFieldLabel} />
     </DivRow>
   )
 }
