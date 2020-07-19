523d76d550ebf6e52c3c1a74b6001888b1181f02
Dang Nghia <dangnghia@Dangs-MacBook-Pro.local>
2020-07-19 08:48:50 +0700

    change position

--- src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
+++ src/components/MainTabs/Tabs/FieldsTab/FieldSelected.js
@@ -1,12 +1,17 @@
 import React, { useState, memo } from 'react';
-import { trashIcon, copyIcon, arrowDownIcon, arrowUpIcon } from '../../../../constants/icons';
+import {
+  trashIcon,
+  copyIcon,
+  arrowDownIcon,
+  arrowUpIcon,
+} from '../../../../constants/icons';
 import { TabPanel, Tabs, TabList, Tab } from 'react-tabs';
 import GeneralContent from './FieldContent/GeneralContent';
 import AdvancedContent from './FieldContent/AdvancedContent';
 import { ucfirst } from '../../../../utility/functions';
 
-const FieldSelected = props => {
-  console.log('zzzz', props)
+const FieldSelected = (props) => {
+  console.log('zzzz', props);
   const type = props.data.general.type;
   const index = props.index;
 
@@ -16,35 +21,64 @@ const FieldSelected = props => {
   if ('divider' === type) {
     return (
       <div
-        data-position={index}
-        draggable
-        onDragStart={props.onDragStart}
-        onDragOver={props.onDragOver}
-        onDrop={props.onDrop}
-        onDragLeave={props.onDragLeave}
-        className={`og-item og-item--${type} og-collapsible${expanded ? ' og-collapsible--expanded' : ''} ${props.dragAndDrop && props.dragAndDrop.draggedTo === index && "dropArea"}`}
-        >
-        <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
-        <Header type={type} index={index} name={props.data.general.name} expanded={expanded} copyItem={props.copyItem} removeItem={props.removeItem} toggleSettings={toggleSettings} />
+        className={`og-item og-item--${type} og-collapsible${
+          expanded ? ' og-collapsible--expanded' : ''
+        } ${
+          props.dragAndDrop &&
+          props.dragAndDrop.draggedTo === index &&
+          'dropArea'
+        }`}
+      >
+        <input
+          ref={props.register}
+          type="hidden"
+          name={`fields-${index}-type`}
+          defaultValue={type}
+        />
+        <Header
+          type={type}
+          index={index}
+          name={props.data.general.name}
+          expanded={expanded}
+          copyItem={props.copyItem}
+          removeItem={props.removeItem}
+          toggleSettings={toggleSettings}
+        />
         <div className="og-item__body og-collapsible__body">
-          <GeneralContent register={props.register} type={type} index={index} fieldData={props.data.general} />
+          <GeneralContent
+            register={props.register}
+            type={type}
+            index={index}
+            fieldData={props.data.general}
+          />
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div
-      data-position={index}
-      draggable
-      onDragStart={props.onDragStart}
-      onDragOver={props.onDragOver}
-      onDrop={props.onDrop}
-      onDragLeave={props.onDragLeave}
-      className={`og-item og-item--${type} og-collapsible${expanded ? ' og-collapsible--expanded' : ''} ${props.dragAndDrop && props.dragAndDrop.draggedTo === index && "dropArea"}`}
-      >
-      <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
-      <Header type={type} index={index} name={props.data.general.name} expanded={expanded} copyItem={props.copyItem} removeItem={props.removeItem} toggleSettings={toggleSettings} />
+      className={`og-item og-item--${type} og-collapsible${
+        expanded ? ' og-collapsible--expanded' : ''
+      } ${
+        props.dragAndDrop && props.dragAndDrop.draggedTo === index && 'dropArea'
+      }`}
+    >
+      <input
+        ref={props.register}
+        type="hidden"
+        name={`fields-${index}-type`}
+        defaultValue={type}
+      />
+      <Header
+        type={type}
+        index={index}
+        name={props.data.general.name}
+        expanded={expanded}
+        copyItem={props.copyItem}
+        removeItem={props.removeItem}
+        toggleSettings={toggleSettings}
+      />
       <div className="og-item__body og-collapsible__body">
         <Tabs forceRenderTabPanel={true}>
           <TabList>
@@ -52,41 +86,89 @@ const FieldSelected = props => {
             <Tab>Advanced</Tab>
           </TabList>
           <TabPanel>
-            { <GeneralContent register={props.register} type={type} index={index} fieldData={props.data.general} /> }
+            {
+              <GeneralContent
+                register={props.register}
+                type={type}
+                index={index}
+                fieldData={props.data.general}
+              />
+            }
           </TabPanel>
           <TabPanel>
-            <AdvancedContent register={props.register} type={type} index={index} data={props.data.advanced} />
+            <AdvancedContent
+              register={props.register}
+              type={type}
+              index={index}
+              data={props.data.advanced}
+            />
           </TabPanel>
         </Tabs>
       </div>
       <div class="og-item__sort">
-        <button type="button" class="og-item__up" title="Move up">{ arrowUpIcon }</button>
-        <button type="button" class="og-item__down" title="Move down">{ arrowDownIcon }</button>
+        <button
+          type="button"
+          class="og-item__up"
+          title="Move up"
+          onClick={() => props.changePosition(props.index, 'up')}
+        >
+          {arrowUpIcon}
+        </button>
+        <button
+          type="button"
+          class="og-item__down"
+          title="Move down"
+          onClick={() => props.changePosition(props.index, 'down')}
+        >
+          {arrowDownIcon}
+        </button>
       </div>
     </div>
-  )
-}
-const Header = props => {
-  const duplicate = e => {
+  );
+};
+const Header = (props) => {
+  const duplicate = (e) => {
     e.stopPropagation();
     props.copyItem(props.type, props.index);
-  }
-  const remove = e => {
+  };
+  const remove = (e) => {
     e.stopPropagation();
     props.removeItem(props.index);
-  }
+  };
 
   return (
-    <div className="og-item__header og-collapsible__header" onClick={props.toggleSettings}>
-      <div className="og-item__title" id={`og-item__title__${props.index}`}>{props.name || ucfirst(props.type)}</div>
+    <div
+      className="og-item__header og-collapsible__header"
+      onClick={props.toggleSettings}
+    >
+      <div className="og-item__title" id={`og-item__title__${props.index}`}>
+        {props.name || ucfirst(props.type)}
+      </div>
       <div className="og-item__actions">
         <span className="og-item__type">{props.type}</span>
-        <span className="og-item__action og-item__action--remove" title="Remove" onClick={remove}>{trashIcon}</span>
-        <span className="og-item__action og-item__action--duplicate" title="Duplicate" onClick={duplicate}>{copyIcon}</span>
-        <span className="og-item__action og-item__action--toggle" title="Toggle Settings">{props.expanded ? arrowUpIcon : arrowDownIcon}</span>
+        <span
+          className="og-item__action og-item__action--remove"
+          title="Remove"
+          onClick={remove}
+        >
+          {trashIcon}
+        </span>
+        <span
+          className="og-item__action og-item__action--duplicate"
+          title="Duplicate"
+          onClick={duplicate}
+        >
+          {copyIcon}
+        </span>
+        <span
+          className="og-item__action og-item__action--toggle"
+          title="Toggle Settings"
+        >
+          {props.expanded ? arrowUpIcon : arrowDownIcon}
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default memo(FieldSelected);
--- src/components/MainTabs/Tabs/FieldsTab/FieldsTab.js
+++ src/components/MainTabs/Tabs/FieldsTab/FieldsTab.js
@@ -5,83 +5,24 @@ import { fields } from '../../../../constants/constants';
 import { getDataCopiedItem } from '../../../../utility/functions';
 import SearchResultList from './SearchResultList';
 
-const initialDnDState = {
-  draggedFrom: null,
-  draggedTo: null,
-  isDragging: false,
-  originalOrder: [],
-  updatedOrder: []
-}
-
 const FieldsTab = (props) => {
   const [selectedList, setListSelected] = useState([]);
   const [searchParam, setSearchParam] = useState('');
-  const [dragAndDrop, setDragAndDrop] = useState(initialDnDState);
 
-  const onDragStart = (event) => {
-    const list = [...selectedList]
-    const id = event.currentTarget.dataset.position
-    const initialPosition = list.map(item => item.data.general.id).indexOf(id);
-
-    setDragAndDrop({
-      ...dragAndDrop,
-      draggedFrom: initialPosition,
-      isDragging: true,
-      originalOrder: selectedList
-    });
-
-    event.dataTransfer.setData("text/html", '');
-  }
-
-  const onDragOver = (event) => {
-    event.preventDefault();
-    let newList = dragAndDrop.originalOrder;
-    const draggedFrom = dragAndDrop.draggedFrom;
-    const list = [...selectedList]
-    const id = event.currentTarget.dataset.position
-    const draggedTo = list.map(item => item.data.general.id).indexOf(id);
-    const itemDragged = newList[draggedFrom];
-    const remainingItems = newList.filter((item, index) => index !== draggedFrom);
-
-    newList = [
-      ...remainingItems.slice(0, draggedTo),
-      itemDragged,
-      ...remainingItems.slice(draggedTo)
-    ];
-
-    if (draggedTo !== dragAndDrop.draggedTo) {
-      setDragAndDrop({
-        ...dragAndDrop,
-        updatedOrder: newList,
-        draggedTo: draggedTo
-      })
-    }
-  }
-
-  const onDragLeave = () => setDragAndDrop({ ...dragAndDrop, draggedTo: null });
-
-  const onDrop = () => {
-    setListSelected(dragAndDrop.updatedOrder);
-
-    setDragAndDrop({
-      ...dragAndDrop,
-      draggedFrom: null,
-      draggedTo: null,
-      isDragging: false
-    });
-  }
-
-  const addItem = type => {
-    const data = { ...fields[type], general: { ...fields[type].general, id: `${type}_${uniqid()}` } };
+  const addItem = (type) => {
+    const data = {
+      ...fields[type],
+      general: { ...fields[type].general, id: `${type}_${uniqid()}` },
+    };
     setListSelected([...selectedList, { type, data }]);
-  }
+  };
 
   const removeItem = (id) => {
     let newList = [...selectedList];
-    const index = newList.map(item => item.data.general.id).indexOf(id);
+    const index = newList.map((item) => item.data.general.id).indexOf(id);
     newList.splice(index, 1);
     setListSelected(newList);
-  }
+  };
 
   const copyItem = (type, id) => {
     let item = { type };
@@ -93,43 +34,64 @@ const FieldsTab = (props) => {
       item.data.general.name += ' Copy';
     }
     let newList = [...selectedList];
-    const index = newList.map(item => item.data.general.id).indexOf(id);
+    const index = newList.map((item) => item.data.general.id).indexOf(id);
     newList.splice(index + 1, 0, item);
     setListSelected(newList);
-  }
-  console.log('sss', selectedList)
+  };
+
+  const changePosition = (id, type) => {
+    let newList = [...selectedList];
+    const index = newList.map((item) => item.data.general.id).indexOf(id);
+    const itemChange = newList[index];
+    if (type === 'up') {
+      newList[index] = newList[index - 1];
+      newList[index - 1] = itemChange;
+    } else {
+      newList[index] = newList[index + 1];
+      newList[index + 1] = itemChange;
+    }
+
+    setListSelected(newList);
+  };
 
   return (
     <div className="og-fields-wrapper">
       <div className="og-sidebar">
-        <input type="search" className="og-search" placeholder="Enter field type here" onChange={e => setSearchParam(e.target.value)} />
-        {searchParam ? <SearchResultList onSelectField={addItem} searchParam={searchParam} /> : <FieldMenu onSelectField={addItem} />}
+        <input
+          type="search"
+          className="og-search"
+          placeholder="Enter field type here"
+          onChange={(e) => setSearchParam(e.target.value)}
+        />
+        {searchParam ? (
+          <SearchResultList onSelectField={addItem} searchParam={searchParam} />
+        ) : (
+          <FieldMenu onSelectField={addItem} />
+        )}
       </div>
 
       <div className="og-main">
-        {selectedList.length === 0 && <p>No fields. Select fields on the left to add them to this field group.</p>}
-        {
-          selectedList.map((item) =>
-            <FieldSelected
-              register={props.register}
-              data={item.data}
-              key={item.data.general.id}
-              index={item.data.general.id}
-              onDragStart={onDragStart}
-              onDragOver={onDragOver}
-              onDrop={onDrop}
-              onDragLeave={onDragLeave}
-              dragAndDrop={dragAndDrop}
-              draggedTo={dragAndDrop.draggedTo}
-              removeItem={removeItem}
-              copyItem={copyItem}
-            />
-          )
-        }
+        {selectedList.length === 0 && (
+          <p>
+            No fields. Select fields on the left to add them to this field
+            group.
+          </p>
+        )}
+        {selectedList.map((item) => (
+          <FieldSelected
+            register={props.register}
+            data={item.data}
+            key={item.data.general.id}
+            index={item.data.general.id}
+            removeItem={removeItem}
+            copyItem={copyItem}
+            changePosition={changePosition}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 const uniqid = () => Math.random().toString(36).substr(2);
 
