c312dd2ce41dd2fe2adeae8c521b751370c2dc59
Anh Tran <anhtnt@elightup.com>
2021-01-04 15:48:52 +0700

    Fix checkbox value

--- src/components/Controls/Checkbox.js
+++ src/components/Controls/Checkbox.js
@@ -3,7 +3,7 @@ import DivRow from './DivRow';
 
 const Checkbox = ( { name, defaultValue, ...rest } ) => (
 	<DivRow htmlFor={ name } { ...rest }>
-		<input type="checkbox" id={ name } name={ name } defaultChecked={ defaultValue } />
+		<input type="checkbox" id={ name } name={ name } defaultChecked={ defaultValue } value={ true } />
 	</DivRow>
 );
 export default Checkbox;
