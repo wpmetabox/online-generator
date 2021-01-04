a9e4262d035a52bcdd70ba264bb36cb954200b3e
Anh Tran <anhtnt@elightup.com>
2021-01-04 15:27:41 +0700

    Update sending data

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,39 +3,39 @@ import Clipboard from 'react-clipboard.js';
 import Highlight from 'react-highlight';
 
 const ResultCode = () => {
-	const [ copied, setCopied ] = useState( false );
-	const [ loading, setLoading ] = useState( false );
+	const [ isGenerating, setIsGenerating ] = useState( false );
 	const [ data, setData ] = useState( '' );
+	const [ copied, setCopied ] = useState( false );
+
+	const onClick = () => {
+		setData( '' );
+		setIsGenerating( true );
+
+		const isTest = window.location.href.includes( 'localhost' );
+		let url = isTest ? 'http://localhost/metaboxio/wp-json/mbb-parser/meta-box' : 'https://metabox.io/wp-json/mbb-parser/meta-box';
+
+		const formData = new FormData( document.querySelector( '#og-form' ) );
+		fetch( url, {
+			method: 'POST',
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: formData
+		} ).then( response => response.json() ).then( response => {
+			setData( response );
+			setIsGenerating( false );
+		} );
+	};
+
 	const copy = () => {
 		setCopied( true );
 		setTimeout( () => setCopied( false ), 1000 );
 	};
 
-	const onClick = e => {
-		e.preventDefault();
-
-		setLoading( true );
-
-		const formData = new FormData( document.querySelector( '#og-form' ) );
-
-		const isTest = window.location.href.includes( 'localhost' );
-		let url = isTest ? 'http://localhost/metaboxio/wp-json/mbb-parser/meta-box' : 'https://metabox.io/wp-json/mbb-parser/meta-box';
-
-		fetch( url, {
-			method: 'POST',
-			body: formData
-		} ).then( response => response.json() ).then( response => {
-			setLoading( false );
-			setData( response );
-		} );
-	};
-
 	return (
 		<>
-			<button type="submit" disabled={ loading } onClick={ onClick }>Generate Code</button>
-			{ loading && <span className="og-loading">Generating code. Please wait...</span> }
+			<button type="button" disabled={ isGenerating } onClick={ onClick }>Generate Code</button>
+			{ isGenerating && <span className="og-loading">Generating code. Please wait...</span> }
 			{
-				data &&
+				data.length > 0 &&
 				<div className="og-result">
 					<div className="alert alert-info">Copy the code and paste into your theme's <code>functions.php</code> file. Wanna more features or use inside the WordPress admin? <a href="https://metabox.io/pricing/" target="_blank" rel="noopener noreferrer">Become a premium user</a>.</div>
 					<div className="og-result__body">
