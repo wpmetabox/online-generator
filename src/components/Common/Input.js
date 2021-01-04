15382bf12606c09396c709f5eea38b5ce548e0df
Anh Tran <anhtnt@elightup.com>
2021-01-04 08:51:47 +0700

    Remove register for common components

--- src/components/Common/Input.js
+++ src/components/Common/Input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import DivRow from './DivRow';
 
-const Input = ({name, defaultValue, register, type = 'text', ...rest}) => (
-  <DivRow htmlFor={name} {...rest}>
-    <input type={type} id={name} name={name} ref={register} defaultValue={defaultValue} />
-  </DivRow>
-)
+const Input = ( { name, defaultValue, type = 'text', ...rest } ) => (
+	<DivRow htmlFor={ name } { ...rest }>
+		<input type={ type } id={ name } name={ name } defaultValue={ defaultValue } />
+	</DivRow>
+);
 
 export default Input;
--- src/components/Common/Textarea.js
+++ src/components/Common/Textarea.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import DivRow from './DivRow';
 
-const Textarea = ({ name, register, defaultValue, ...rest }) => (
-  <DivRow {...rest} htmlFor={name}>
-    <textarea ref={register} defaultValue={defaultValue} id={name} name={name} rows="2"></textarea>
-  </DivRow>
-)
+const Textarea = ( { name, defaultValue, ...rest } ) => (
+	<DivRow { ...rest } htmlFor={ name }>
+		<textarea defaultValue={ defaultValue } id={ name } name={ name } rows="4"></textarea>
+	</DivRow>
+);
 export default Textarea;
