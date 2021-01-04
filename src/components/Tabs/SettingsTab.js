10ffe76a42c4c5154e42d88cb6e15379fe3282f6
Anh Tran <anhtnt@elightup.com>
2021-01-04 15:39:38 +0700

    Wrap settings fields inside an array

--- src/components/Tabs/SettingsTab.js
+++ src/components/Tabs/SettingsTab.js
@@ -6,14 +6,14 @@ import Input from '../Controls/Input';
 const SettingsTab = () => (
 	<>
 		<h3>Options</h3>
-		<Input name="title" label="Meta box title" defaultValue="Untitled" />
-		<Input name="id" label="Meta box ID" defaultValue="untitled" />
+		<Input name="post_title" label="Field group title" defaultValue="Untitled Field Group" />
+		<Input name="post_name" label="Field group ID" defaultValue="untitled" />
 		<DivRow label="Post types" className="og-field--check">
-			<label><input type="checkbox" name="post_types[]" value="post" defaultChecked /> Post</label>
-			<label><input type="checkbox" name="post_types[]" value="page" /> Page</label>
+			<label><input type="checkbox" name="settings[post_types][]" value="post" defaultChecked /> Post</label>
+			<label><input type="checkbox" name="settings[post_types][]" value="page" /> Page</label>
 		</DivRow>
 		<DivRow label="Position" htmlFor="context">
-			<select id="context" name="context" defaultValue="normal">
+			<select id="context" name="settings[context]" defaultValue="normal">
 				<option value="normal">After content</option>
 				<option value="side">Side</option>
 				<option value="form_top">Before post title</option>
@@ -21,13 +21,13 @@ const SettingsTab = () => (
 			</select>
 		</DivRow>
 		<DivRow label="Priority" className="og-field--check">
-			<label><input type="radio" name="priority" value="high" defaultChecked /> High</label>
-			<label><input type="radio" name="priority" value="low" /> Low</label>
+			<label><input type="radio" name="settings[priority]" value="high" defaultChecked /> High</label>
+			<label><input type="radio" name="settings[priority]" value="low" /> Low</label>
 		</DivRow>
-		<Checkbox name="autosave" label="Autosave" />
+		<Checkbox name="settings[autosave]" label="Autosave" />
 		<h3>Advanced</h3>
-		<Input name="prefix" label="Field ID prefix" defaultValue="prefix-" description="Prefix for all fields' ID. Leave empty to ignore or use _ to make fields hidden." tooltip="Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins." />
-		<Input name="text_domain" label="Text domain" defaultValue="online-generator" tooltip="Required for multilingual website. Used in the exported code only." />
+		<Input name="settings[prefix]" label="Field ID prefix" defaultValue="" description="Prefix for all fields' ID. Leave empty to ignore or use _ to make fields hidden." tooltip="Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins." />
+		<Input name="settings[text_domain]" label="Text domain" defaultValue="online-generator" tooltip="Required for multilingual website. Used in the exported code only." />
 	</>
 );
 export default SettingsTab;
